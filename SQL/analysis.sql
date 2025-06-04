81c32806588c3c5983c8fc2798195ee48f9a5d4b
deepa905 <deepatk916@gmail.com>
2025-06-04 22:11:52 +0530

    business_queries.sql

--- SQL/analysis.sql
+++ SQL/analysis.sql
@@ -1,8 +1,18 @@
--- Top 10 profitable products
+-- 1. Customer Segmentation (Top 5 Customers by Sales)
 SELECT 
-    Product_Name, 
-    SUM(Profit) AS Total_Profit
+    Customer_Name, 
+    SUM(Sales) AS Total_Sales,
+    COUNT(Order_ID) AS Order_Count
+FROM sales
+GROUP BY Customer_Name
+ORDER BY Total_Sales DESC
+LIMIT 5;
+
+-- 2. Product Performance (High Sales but Low Profit)
+SELECT 
+    Product_Name,
+    SUM(Sales) AS Total_Sales,
+    AVG(Profit_Margin) AS Avg_Profit_Margin
 FROM sales
 GROUP BY Product_Name
-ORDER BY Total_Profit DESC
-LIMIT 10;
+HAVING Avg_Profit_Margin < 10 AND Total_Sales > 5000;
